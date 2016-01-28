72fe0b45127a632dfbdbe2d7c8e1225318381680
Maxime Ripard <maxime.ripard@free-electrons.com>
2016-01-28 09:12:35 +0100

    EEPROM: Add an EEPROM uclass
    We might want to access data stored onto EEPROMs. Create a framework to
    provide a consistent API.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- include/dm/uclass-id.h
+++ include/dm/uclass-id.h
@@ -83,6 +83,7 @@ enum uclass_id {
 	UCLASS_VIDEO_BRIDGE,	/* Video bridge, e.g. DisplayPort to LVDS */
 	UCLASS_VIDEO_CONSOLE,	/* Text console driver for video device */
 	UCLASS_W1,		/* Dallas 1-Wire bus */
+	UCLASS_EEPROM,		/* EEPROM */
 
 	UCLASS_COUNT,
 	UCLASS_INVALID = -1,
